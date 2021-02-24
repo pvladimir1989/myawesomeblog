b6fe20291733939f2083321d147e18e853b495d1
pvladimir1989 <vpokutniy@inbox.ru>
2021-02-24 21:23:26 +0300

    project finished

--- events/models.py
+++ events/models.py
@@ -6,3 +6,5 @@ class Event(models.Model):
     event_text=models.CharField(max_length=300)
 
 
+
+
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -77,10 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myawesomeblogdb',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myawesomeblog/static/')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL='/awesome_media/'
+MEDIA_URL = '/awesome_media/'
